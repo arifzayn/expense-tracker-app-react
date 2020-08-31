c3a3b368f2d032160276609d70068ae63c727997
arifzayn <60439783+arifzayn@users.noreply.github.com>
2020-08-31 23:18:05 +0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -12,11 +12,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://twitter.com/zayn_css"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Zen
         </a>
       </header>
     </div>
